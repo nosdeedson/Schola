8b5b53c18dd9dbc48f1e0466364d2409d8e65493
nosdeEdson <josemariazumira@gmail.com>
2026-06-07 13:42:56 -0300

    created parentstudent mapper

--- src/domain/@shared/repository/person.repository.interface.ts
+++ src/domain/@shared/repository/person.repository.interface.ts
@@ -1,4 +1,5 @@
 import { PersonEntity } from "@/infrastructure/entities/@shared/person.entity";
 import { RepositoryInterface } from "./repository.interface";
+import { Person } from "../person";
 
-export interface PeronRepositoryInterface<T> extends RepositoryInterface<PersonEntity> { }
+export interface PeronRepositoryInterface<T> extends RepositoryInterface<PersonEntity, Person> { }
