2fb09e7fdcb75ed8d81f6ce782d65ab9ec29a77e
nosdeEdson <josemariazumira@gmail.com>
2026-06-07 13:45:03 -0300

    fixed the quarter interface

--- src/domain/quarter/quarter.repository.interface.ts
+++ src/domain/quarter/quarter.repository.interface.ts
@@ -1,5 +1,6 @@
 import { QuarterEntity } from "@/infrastructure/entities/quarter/quarter.entity";
 import { RepositoryInterface } from "../@shared/repository/repository.interface";
+import { Quarter } from "./quarter";
 
-export interface QuarterRepositoryInterface extends RepositoryInterface<QuarterEntity> {
+export interface QuarterRepositoryInterface extends RepositoryInterface<QuarterEntity, Quarter> {
 }
